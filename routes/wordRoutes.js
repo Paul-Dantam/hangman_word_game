165af818a11d3a42b66edc398bef1bf713612494
Paul Dantam <pauldantam@Pauls-MacBook-Pro.local>
2017-09-02 14:19:48 -0500

    game basic refactoring

--- routes/wordRoutes.js
+++ routes/wordRoutes.js
@@ -10,52 +10,66 @@ const words = fs
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+function lossResponse() {
+  let response = window.confirm("Do you want to play again?");
+  if (response === true) res.redirect("/words/newgame");
+  else res.redirect("/");
+}
 
 wordRoutes.get("/newgame", (req, res) => {
   const randomWord = words[getRandomInt(0, words.length - 1)];
   console.log("randomWord: ", randomWord);
   req.session.word = randomWord;
-  res.redirect("/");
-
   let displayArray = [];
   for (let i = 0; i < randomWord.length; i++) {
-    displayArray.push("_");
+    displayArray.push("_ ");
   }
   req.session.display = displayArray;
   req.session.wrongGuesses = [];
   req.session.correctGuesses = [];
   req.session.turns = 8;
+  req.session.msg = "Good Luck, you can miss " + req.session.turns + " guesses";
   res.redirect("/");
 });
 
-wordRoutes.post("/", (req, res) => {
+wordRoutes.post("/guess", (req, res) => {
   let guessLetter = req.body.letter.toUpperCase();
   let word = req.session.word;
+  console.log(req.session.wrongGuesses, req.session.correctGuesses);
 
   if (
     req.session.wrongGuesses.indexOf(guessLetter) > -1 ||
-    req.session.correctGuesses(indexOf)
+    req.session.correctGuesses.indexOf(guessLetter) > -1
   ) {
-    req.session.msg = "you already guessed that letter.";
+    req.session.msg =
+      "you already guessed that letter, you have " +
+      req.session.turns +
+      " turns left.";
     return res.redirect("/");
-  }
-
-  if (word.indexOf(guessLetter) < 0) {
-    req.session.msg = "Wrong";
-    req.session.wrongGuesses.push(guessLetter);
+  } else if (word.indexOf(guessLetter) < 0) {
     req.session.turns -= 1;
+    if (req.session.turns < 1) {
+      req.session.msg = "no more turns. Game Over";
+    }
+    req.session.msg = "Wrong, you have " + req.session.turns + " turns left";
+    req.session.wrongGuesses.push(guessLetter);
+    return res.redirect("/");
     if (req.session.turns < 1) {
       req.session.msg = "no more turns. Game Over";
       return res.redirect("/");
     }
-  }
-
-  for (let i = 0; i < word.length; i++) {
-    if (word.charAt(i) === guessLetter) {
-      req.session.msg = null;
-      display[i] = guessLetter;
+  } else
+    for (let i = 0; i < word.length; i++) {
+      if (word.charAt(i).toUpperCase() === guessLetter) {
+        req.session.msg =
+          "Correct , you have " + req.session.turns + " turns left";
+        req.session.display[i] = guessLetter;
+        if (req.session.correctGuesses.indexOf(guessLetter) < 0) {
+          req.session.correctGuesses.push(guessLetter);
+        }
+      }
     }
-  }
+  return res.redirect("/");
 });
 
 module.exports = wordRoutes;
--- server.js
+++ server.js
@@ -4,21 +4,16 @@ const session = require("express-session");
 const logger = require("morgan");
 const mustacheExpress = require("mustache-express");
 const path = require("path");
-const users = require("./data");
-
-const checkAuth = require("./middlewares/checkAuth");
-const userParser = require("./middlewares/userParser");
-const sessionConfig = require("./sessionConfig");
-const gameBuild = require("./middlewares/gameBuild");
-const indexRoutes = require("./routes/indexRoutes");
-const authRoutes = require("./routes/authRoutes");
-const userRoutes = require("./routes/userRoutes");
-const app = express();
-
+const fs = require("fs");
+const wordRoutes = require("./routes/wordRoutes");
 const words = fs
   .readFileSync("/usr/share/dict/words", "utf-8")
-  .toLowerCase()
+  .toUpperCase()
   .split("\n");
+
+const sessionConfig = require("./sessionConfig");
+const indexRoutes = require("./routes/indexRoutes");
+const app = express();
 
 const port = process.env.PORT || 8000;
 
@@ -27,16 +22,13 @@ app.set("views", "./views");
 app.set("view engine", "mustache");
 
 app.use(express.static(path.join(__dirname, "public")));
-app.use(logger("dev"));
+// app.use(logger("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(session(sessionConfig));
-app.use(userParser);
-app.use(gameBuild);
 
 //routes
 app.use("/", indexRoutes);
-app.use("/auth", authRoutes);
-app.use("/user", checkAuth, userRoutes);
+app.use("/word", wordRoutes);
 
 app.listen(port, () => {
   console.log(`Word game server running on port ${port}.`);
